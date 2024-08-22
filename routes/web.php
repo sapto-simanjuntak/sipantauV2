52d1090dda7b6c81c183498a0d2e62ba242d6ae1
sapto-simanjuntak <saptosimanjuntak@gmail.com>
2024-08-22 14:01:32 +0700

    nambahkan auth pada middelware route

--- routes/web.php
+++ routes/web.php
@@ -51,62 +51,50 @@ Route::get('/', function () {
 });
 
 // Auth::routes();
+// Route::resource('permission', PermissionController::class);
+// Route::delete('/permission/{id}', [PermissionController::class, 'delete'])->name('permission.delete');
+// Route::resource('role', RoleController::class);
+// Route::delete('/role/{id}', [RoleController::class, 'delete'])->name('role.delete');
+// Route::resource('user', UserController::class);
+// Route::delete('/user/{id}', [UserController::class, 'delete'])->name('user.delete');
+// Route::get('user/{id}/roles', [App\Http\Controllers\Akses\UserController::class, 'getUserRoles'])->name('user.roles');
+// Route::get('role/{roleId}/give-permissions', [App\Http\Controllers\Akses\RoleController::class, 'addPermissionToRole']);
+// Route::put('role/{roleId}/give-permissions', [App\Http\Controllers\Akses\RoleController::class, 'givePermissionToRole']);
+// Route::get('get-roles', [App\Http\Controllers\Global\SelectController::class, 'selectRoles']);
+// Route::get('get-user', [App\Http\Controllers\Global\SelectController::class, 'selectUser']);
+// Route::get('dokter-autocomplete-search', 'RegistrasiController@selectDokter');
+// Route::resource('projects', ProjectController::class);
+// Route::delete('/projects/{id}', [ProjectController::class, 'delete'])->name('projects.delete');
+// Route::put('/projects/{id}', [ProjectController::class, 'update'])->name('add-pic');
+// Route::post('/projects/{project}/add-pic', [ProjectController::class, 'addPic'])->name('projects.addPic');
+// Route::post('/projects/add-pic', [ProjectController::class, 'addPic'])->name('projects.addPic');
+// Route::post('/projects/delete-pic', [ProjectController::class, 'deletePic'])->name('projects.deletePic');
+// Route::post('/projects/add-validasi', [ProjectController::class, 'addValidasi'])->name('projects.addValidasi');
+// Route::get('project/{id}/give-task', [App\Http\Controllers\Project\ProjectController::class, 'addTask']);
+// Route::get('project/{id}/give-task', [ProjectController::class, 'giveTask'])->name('project.give-task');
+// Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
+// Route::post('register', [AuthController::class, 'register']);
+// Route::resource('projects.tasks', TaskController::class)->shallow();
+
+
+// Route::resource('tasks', TaskController::class);
+// Route::resource('comments', CommentController::class);
 
 Route::group(
-    ['middleware' => ['role:Superadmin|Admin|User|Unit']],
+    ['middleware' => ['auth', 'role:Superadmin|Admin|User|Unit']],
     function () {
-        // Route::resource('permission', PermissionController::class);
-        // Route::delete('/permission/{id}', [PermissionController::class, 'delete'])->name('permission.delete');
-        // Route::resource('role', RoleController::class);
-        // Route::delete('/role/{id}', [RoleController::class, 'delete'])->name('role.delete');
-        // Route::resource('user', UserController::class);
-        // Route::delete('/user/{id}', [UserController::class, 'delete'])->name('user.delete');
-        // Route::get('user/{id}/roles', [App\Http\Controllers\Akses\UserController::class, 'getUserRoles'])->name('user.roles');
         Route::resource('data', DataController::class);
-
         Route::delete('/data/{id}', [DataController::class, 'delete'])->name('data.delete');
-        // Route::get('role/{roleId}/give-permissions', [App\Http\Controllers\Akses\RoleController::class, 'addPermissionToRole']);
-        // Route::put('role/{roleId}/give-permissions', [App\Http\Controllers\Akses\RoleController::class, 'givePermissionToRole']);
-
-        // Route::get('get-roles', [App\Http\Controllers\Global\SelectController::class, 'selectRoles']);
-
-        // Route::get('get-user', [App\Http\Controllers\Global\SelectController::class, 'selectUser']);
-
-        // Route::get('dokter-autocomplete-search', 'RegistrasiController@selectDokter');
-
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
         Route::get('/reports', [DashboardController::class, 'showReports'])->name('reports');
-
         Route::get('/reports/monthly', [DashboardController::class, 'showMonthlyReport'])->name('reports.monthly');
-
-        // Route::resource('projects', ProjectController::class);
-        // Route::delete('/projects/{id}', [ProjectController::class, 'delete'])->name('projects.delete');
-
-        // Route::put('/projects/{id}', [ProjectController::class, 'update'])->name('add-pic');
-
-        // Route::post('/projects/{project}/add-pic', [ProjectController::class, 'addPic'])->name('projects.addPic');
-        // Route::post('/projects/add-pic', [ProjectController::class, 'addPic'])->name('projects.addPic');
-        // Route::post('/projects/delete-pic', [ProjectController::class, 'deletePic'])->name('projects.deletePic');
-        // Route::post('/projects/add-validasi', [ProjectController::class, 'addValidasi'])->name('projects.addValidasi');
-
-        // Route::get('project/{id}/give-task', [App\Http\Controllers\Project\ProjectController::class, 'addTask']);
-        // Route::get('project/{id}/give-task', [ProjectController::class, 'giveTask'])->name('project.give-task');
         Route::get('formulir/{id}/view-task', [FormulirController::class, 'giveTask'])->name('formulir.view-task');
-
-        // Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
-        // Route::post('register', [AuthController::class, 'register']);
-
-
-        // Route::resource('tasks', TaskController::class);
-        // Route::resource('comments', CommentController::class);
         Route::resource('formulir', FormulirController::class);
-        // Route::resource('projects.tasks', TaskController::class)->shallow();
     }
 );
 
 Route::group(
-    ['middleware' => ['role:Superadmin|Admin|User']],
+    ['middleware' => ['auth', 'role:Superadmin|Admin|User']],
     function () {
         Route::resource('data', DataController::class);
 
@@ -142,7 +130,7 @@ Route::group(
 );
 
 Route::group(
-    ['middleware' => ['role:Superadmin']],
+    ['middleware' => ['auth', 'role:Superadmin']],
     function () {
         Route::resource('permission', PermissionController::class);
         Route::delete('/permission/{id}', [PermissionController::class, 'delete'])->name('permission.delete');
